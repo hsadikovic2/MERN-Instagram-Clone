47ae5226733b82b8d22dbd359b18a914f003334e
Haris Sadiković <hsadikovic2@etf.unsa.ba>
2025-12-17 17:31:26 +0100

    Add Post schema and Create post route

--- server/app.js
+++ server/app.js
@@ -3,22 +3,26 @@ const app = express();
 const mongoose = require('mongoose');
 const PORT = 5000;
 const {MONGOURI} = require('./keys');
-require('./models/User');
-//app usings
-app.use(express.json());
-app.use(require('./routes/auth'));
+
 //connection to database
 mongoose.connect(MONGOURI);
 mongoose.connection.on('connected', () => {
     console.log("Connected to mongoo");
 })
-
 mongoose.connection.on('error', err => {
     console.log("err connecting",err);
 })
 
+//app requiring models
+require('./models/User');
+require('./models/Post');
 
+//app usings
+app.use(express.json());
+app.use(require('./routes/auth'));
+app.use(require('./routes/post'));
 
+//server listening
 app.listen(PORT, () => {
     console.log("Server is running on port 5000");
 })
